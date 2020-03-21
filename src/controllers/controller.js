d0324b729c89d88719bd2e4a85b3c859808df60b
Jethet <henriette.hettinga@gmail.com>
2020-03-21 12:21:01 +0100

    Create Weather model, error condition for input

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,16 +1,35 @@
 const axios = require("axios");
 const dotenv = require("dotenv").config();
 
+const Weather = require("../Weather");
+
 exports.renderHomePage = (req, res) => {
   res.render("index");
 };
 
 exports.getWeather = (req, res) => {
-  console.log(req);
-
   const city = req.body.city;
-};
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}&units=metric`;
 
-exports.renderAboutPage = (req, res) => {
-  res.render("about");
+  const weather = new Weather(req.body.city);
+  weather.validateUserInput();
+
+  if (weather.errors.length) {
+    res.render("index", {
+      error: weather.errors.toString()
+    });
+  } else {
+    axios
+      .get(url)
+      .then(response =>
+        res.render("index", {
+          weather: `It is currently ${response.data.main.temp} in ${response.data.name}.`
+        })
+      )
+      .catch(err => console.log(err));
+  }
+
+  exports.renderAboutPage = (req, res) => {
+    res.render("about");
+  };
 };
